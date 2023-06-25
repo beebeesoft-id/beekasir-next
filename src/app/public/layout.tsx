14fa61849eac54db8774c974900a4e2dafcfdb41
Kiki Kiswanto <kiki.kiswanto22@gmail.com>
2023-06-25 10:44:20 +0700

    [APP] Logo Beekasir

--- src/app/public/layout.tsx
+++ src/app/public/layout.tsx
@@ -15,10 +15,10 @@ export default function PublicLayout({
         <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
             <div className="flex lg:flex-1">
             <Link href="/" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
+                <span className="sr-only">BeeKasir</span>
                 <Image
-              src="../../beebeesoft.svg"
-              alt="Beebeesoft Logo"
+              src="../../beekasir-logo.svg"
+              alt="Beekasir Logo"
               className="dark:invert"
               width={50}
               height={24}
@@ -55,10 +55,10 @@ export default function PublicLayout({
             <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                 <div className="flex items-center justify-between">
                     <Link href="#" className="-m-1.5 p-1.5">
-                    <span className="sr-only">Your Company</span>
+                    <span className="sr-only">BeeKasir</span>
                     <Image
-                    src="../../beebeesoft.svg"
-                    alt="Beebeesoft Logo"
+                    src="../../beekasir-logo.svg"
+                    alt="BeeKasir Logo"
                     className="dark:invert"
                     width={50}
                     height={24}
