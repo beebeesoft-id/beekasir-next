d5337fc4fb0854fd801606626dea31e1f58f9dfb
Kiki Kiswanto <kiki.kiswanto22@gmail.com>
2023-10-22 14:48:21 +0700

    Opt - Out Of Memory

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    modularizeImports: {
+        "@mui/material/?(((\\w*)?/?)*)": {
+          transform: "@mui/material/{{ matches.[1] }}/{{member}}",
+        },
+        "@mui/icons-material/?(((\\w*)?/?)*)": {
+          transform: "@mui/icons-material/{{ matches.[1] }}/{{member}}",
+        },
+      },
+}
 
 module.exports = nextConfig
