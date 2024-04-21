2df0ddb580cb4738ca524d53ecd67bc97f6642d1
Kiki Kiswanto <kiki.kiswanto22@gmail.com>
2024-04-21 15:08:11 +0700

    Pay - Page Receipt Status

--- src/app/public/payment/page.tsx
+++ src/app/public/payment/page.tsx
@@ -1,12 +1,64 @@
+"use client"
+import { faFileInvoiceDollar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Badge, Button, Card, CardActions, CardContent, Typography } from "@mui/material";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Pricing() {
+    const [orderId, setOrderId] = useState<any>('');
+    const [transactionStatus, setTransactionStatus] = useState<any>('');
+    const param = useSearchParams();
+
+    useEffect(() => {
+      getData();
+    
+      return () => {
+        
+      }
+    }, [])
+
+    async function getData() {
+
+        const o : string | null = param.get('order_id');
+        const t : string | null = param.get('transaction_status');
+        
+        setOrderId(o);
+        setTransactionStatus(t);
+        
+    }
+    
+
     return (
-        <div className="bg-white py-24 sm:py-32">
-            <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                <div className="mx-auto max-w-2xl sm:text-center">
-                <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Payment Confirmation</h2>
-                <p className="mt-6 text-lg leading-8 text-gray-600">Processing Payment</p>
-                </div>
+        <div className="bg-white py-24">
+            <div className="mx-auto max-w-7xl px-6">
+            <Card sx={{ minWidth: 275, textAlign:'center' }}>
+                <CardContent>
+                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    BeeKasir
+                    </Typography>
+                    <Typography variant="h5" component="div">
+                    
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    Pembayaran Member Berlangganan
+                    </Typography>
+                    
+                    <Badge badgeContent={ transactionStatus } color="success">
+                        <FontAwesomeIcon icon={faFileInvoiceDollar} color={'black'} size={'10x'}/>
+                    </Badge>
+                    <br /> <br />
+                    <Typography variant="body2">
+                    Order ID : { orderId }
+                    <br />
+                    {((transactionStatus == 'settlement') || transactionStatus == 'capture') ? 'Status Member Telah Aktif Silahkan cek Pada aplikasi' : 'Status Pembayaran akan dikirim ke email'}
+                    <br />
+                    </Typography>
+                </CardContent>
+                {/* <CardActions>
+                    <Button size="small">Learn More</Button>
+                </CardActions> */}
+            </Card>
             </div>
         </div>
 
