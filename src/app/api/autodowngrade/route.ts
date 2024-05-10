87f59afead68ffc28d192404ceb2a6f02b57fabd
Kiki Kiswanto <kiki.kiswanto22@gmail.com>
2024-05-10 09:22:27 +0700

    Sch - Auto Reminder 5 Days

--- src/app/api/autodowngrade/route.ts
+++ src/app/api/autodowngrade/route.ts
@@ -28,12 +28,14 @@ export async function GET(req : Request, response : Response, head : Headers) {
         
       const today = moment().format('YYYY-MM-DD');
       console.log('Found ' + data.length);
-      
+      let countExp = 0;
       data.forEach(async(val) => {
         let row = val.data();
         let expired = false;
+        remider(row);
         if (today > row.exp) {
             expired = true;
+            countExp = countExp+1;
             const refCompany = doc(DB, "Company/" + row.id);
             await updateDoc(refCompany, { 'level' : 0 });
             console.log(row.id + ' - ' + row.companyName + ' - Expired');
@@ -42,7 +44,7 @@ export async function GET(req : Request, response : Response, head : Headers) {
       });
       console.log("Finish Scanning");
       
-      return NextResponse.json({ 'data': data.length, 'status': '200', 'statusDesc' : 'Member Berhasil di scaning'});
+      return NextResponse.json({ 'data': countExp + ' of ' + data.length, 'status': '200', 'statusDesc' : 'Member Berhasil di scanning'});
     } catch (error) {
         console.log("Error di autodowngrade");
         
@@ -55,4 +57,59 @@ export async function GET(req : Request, response : Response, head : Headers) {
     }
   
   
+}
+
+async function remider(data : any) {
+  let selisih = 0;
+  const dayReminder = 5;
+  if (data.exp) {
+    selisih = moment().diff(data.exp, 'day');
+  }
+  if (selisih == dayReminder) {
+    sendEmail(data);
+    console.log('Kirim reminder ke ' + data.createdBy);
+  }
+  
+  
+}
+
+const sendEmail = async(data : any) => {
+  try {
+      const response = await fetch('https://api.mailersend.com/v1/email',
+      {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer mlsn.920845eec581dad1c94108f4890cd3ff1b424e9a9b9e00f363b179cbe85a9d59',
+            'X-Requested-With' : 'XMLHttpRequest'
+          },
+          body: JSON.stringify({
+              "from": {
+                  "email": 'admin@beebeesoft.com',
+                  "name": 'Beekasir System'
+              },
+              "to": [
+                  {
+                      "email": data.createdBy,
+                      "name": data.companyName
+                  }
+              ],
+              "cc": [
+                {
+                    "email": 'beebeesoft.id@gmail.com',
+                    "name": 'BeeBeeSoft'
+                }
+              ],
+                  "subject": "Reminder Beekasir: Perpanjangan " + data.exp,
+                  "text":"Hi Saatnya perpanjangan, akun beekasir anda akan expired nih, lakukan perpanjangan pada aplikasi beekasir pojok kanan atas di halaman home ya.",
+                  "html": `Hi ${data.companyName} <br/><br/>Saatnya perpanjangan, akun Beekasir anda akan expired nih di ${data.exp}, lakukan perpanjangan pada aplikasi beekasir pojok kanan atas di halaman home ya. <br/>Email ini dikirim otomatis no reply ya kak<br/>Kirim email ke beebeesoft.id@gmail.com jika ada kendala. <br/><br/>Salam<br/>Beekasir System`
+          }),
+      });
+      console.log(response);
+      
+  } catch (error) {
+      //console.error(error);
+      
+      console.log(error)
+  }
 }
