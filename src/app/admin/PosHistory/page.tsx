f8a374a3a1f60ac48b45d75f4a357b1c2897bc14
Kiki Kiswanto <kiki.kiswanto22@gmail.com>
2024-06-25 06:59:38 +0700

    Pos History

--- src/app/admin/PosHistory/page.tsx
+++ src/app/admin/PosHistory/page.tsx
@@ -1,27 +1,36 @@
 "use client"
 import { DB, refProduct, refTrx } from "@/service/firebase";
 import { Trx } from "@/service/model";
-import { Card, CardContent, Grid } from "@mui/material";
-import { DocumentData, collection, getDocs } from "firebase/firestore";
+import { Button, Card, CardContent, Grid } from "@mui/material";
+import { DocumentData, collection, getDocs, orderBy, query, where } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid'
 import { ConfirmSweet, localRemove, localSave } from "@/service/helper";
 import { useRouter } from "next/navigation";
+import moment from "moment";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function PosTrx() {
     const [listTrx, setListTrx] = useState<Trx[]>([]);
     const router = useRouter();
+    const [lastUpdated, setLastUpdated] = useState('HH:ii:ss');
 
     const columns : GridColDef[] = [
+        {
+            field: 'id' , 
+            headerName: 'No', 
+            filterable: false,
+            renderCell:(index) => index.api.getAllRowIds().indexOf(index.row.trxId) + 1
+        },
         { field: 'trxId', headerName : 'No Nota', width:120},
         { field: 'createdDate', headerName : 'Waktu', width:200},
-        { field: 'trxCost', headerName : 'Modal', type:"number"},
-        { field: 'trxTotal', headerName : 'Pendapatan', type:"number"},
+        { field: 'trxTotal', headerName : 'Total', type:"number"},
         { field: 'trxQty', headerName : 'Qty', type:"number"},
         { field: 'method', headerName : 'Pembayaran'},
         { field: 'status', headerName : 'Status'},
         { field: 'kasir', headerName : 'Kasir', width:150},
         { field: 'memberName', headerName : 'Member'},
+        { field: 'note', headerName : 'Catatan'},
     ]
 
     useEffect(() => {
@@ -35,13 +44,13 @@ export default function PosTrx() {
     function getList() {
         
         const ref = collection(DB, refTrx());
-        getDocs(ref).then((response) => {
+        getDocs(query(ref, orderBy('createdDate','desc'))).then((response) => {
             let data : any = response.docs.map((d) => {
                 return d.data();
             });
             setListTrx(data);
             console.log(data);
-            
+            setLastUpdated(moment().format('HH:mm:ss'));
 
         })
     }
@@ -55,17 +64,20 @@ export default function PosTrx() {
     }
     return (
         <>
+        <br />
         <Grid container spacing={2}>
-            {/* <Grid item sm={12} md={3} xl={3}>
-                <Card>
-                    <CardContent>
-                        Pencarian
-                    </CardContent>
-                </Card>
-            </Grid> */}
+            
             <Grid item sm={12} md={12} xl={12}>
                 <Card>
                     <CardContent>
+                    <Grid container>
+                        <Grid item sm={6} md={6} xl={6}>
+                            Total Data : { listTrx.length }
+                        </Grid>
+                        <Grid item sm={6} md={6} xl={6} className="text-right">
+                            <i>Last Update : { lastUpdated }</i> <Button type="button" variant="outlined" onClick={getList}> <FontAwesomeIcon icon={'rotate'}/> Refresh </Button>
+                        </Grid>
+                    </Grid>
                         <DataGrid
                         rows={listTrx}
                         columns={columns}
