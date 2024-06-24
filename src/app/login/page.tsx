e5edce1b49e4943b31aadca96b45824070227429
Kiki Kiswanto <kiki.kiswanto22@gmail.com>
2024-06-24 20:56:21 +0700

    hide logo on login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -151,15 +151,17 @@ export default function Login() {
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <Link href={'/'}>
-            <img
-              className="mx-auto h-10 w-auto"
+            {/* <img
+
+              className="mx-auto w-auto center"
               src="/beekasir-logo.svg"
               alt="Your Company"
-            />
-            </Link>
+            /> */}
+            
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
               Sign in to Beekasir Web
             </h2>
+            </Link>
           </div>
   
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
